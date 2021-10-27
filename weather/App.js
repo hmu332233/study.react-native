775235335c752d86626db68d245e8697fd97b646
minung.han <hmu332233@gmail.com>
2021-10-28 00:01:47 +0900

    기본 구조 설명 주석 추가

--- weather/App.js
+++ weather/App.js
@@ -6,13 +6,17 @@ export default function App() {
   return (
     // View - container 역할
     <View style={styles.container}> 
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* 모든 text는 Text 컴포넌트에 들어가야함 */}
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <Text>Hello!</Text>
-      <StatusBar style="auto" />
+      {/* 실제로 화면에 표시되는게 아닌 시스템 상태바와 소통할 수 있는 수단 */}
+      <StatusBar style="auto" /> 
     </View>
   );
 }
 
+// style의 object를 만듦
+// StyleSheet.create을 통해서 만들면 자동완성을 편하게 사용 가능
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,4 +24,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'red',
+  }
 });
